cfb64ff2eab5ea8327b66eb767ef5a507762087a
Jiehfeng <alainjiehfeng@gmail.com>
2023-02-13 21:07:43 +0530

    Made massive changes that include feature introductions.

--- application.py
+++ application.py
@@ -1,14 +1,32 @@
 import json
+import traceback
+
+import psycopg2
+import psycopg2.extras
 from flask import Flask, jsonify, render_template, request, Response, send_file
+
+# Lyrical Similarity
+from difflib import SequenceMatcher
 
 # PEGASUS Transformer Model from Google Researchers (Liu & Zhao, 2020)
 import torch
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
+import warnings
+warnings.filterwarnings("ignore")
+
 model_name = "tuner007/pegasus_paraphrase"
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+
+
+# Database
+conn = psycopg2.connect(database="song_database",
+                        host="localhost",
+                        user="jiehfeng",
+                        password="jiehfeng@iit.ac.lk",
+                        port="5432")
 
 
 def paraphrase_lyrics(lyrics, number_of_variations):
@@ -36,8 +54,150 @@ def home_view():
     return "<h1>Welcome to Song Searcher</h1>"
 
 
-@application.route("/lyrics/paraphrase")
+@application.route("/admin/submit-song")
+def submit_song():
+    admin_key = request.headers.get("ADMIN-KEY")
+
+    if admin_key == 'jiehfeng@iit.ac.lk':
+        title = request.args.get("title")
+        artist = request.args.get("artist")
+        lyrics = request.args.get("lyrics")
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        '''cur.execute("CREATE TABLE songs ("
+                       "song_no serial PRIMARY KEY,"
+                       "title VARCHAR NOT NULL,"
+                       "artist VARCHAR NOT NULL,"
+                       "other JSON,"
+                       "lyrics JSON NOT NULL"
+                       ")")'''
+
+        formatted_lyrics = json.dumps({"Lyrics": lyrics})
+
+        try:
+            cur.execute("INSERT INTO songs (title, artist, lyrics) VALUES (%s, %s, %s)", (title,
+                                                                                          artist,
+                                                                                          formatted_lyrics))
+            conn.commit()
+        except:
+            print('[Song Searcher] - Sorry, song submission failed. Try again. (ERROR BELOW)')
+            traceback.print_exc()
+            cur.close()
+            conn.rollback()
+
+    return "OK"
+
+
+@application.route("/admin/get-song")
+def get_song():
+    admin_key = request.headers.get("ADMIN-KEY")
+
+    if admin_key == 'jiehfeng@iit.ac.lk':
+        title = request.args.get("title")
+        artist = request.args.get("artist")
+        lyrics = request.args.get("lyrics")
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        sent = {
+            "Title": title,
+            "Artist": artist,
+        }
+
+        if sent["Title"]:
+            try:
+                cur.execute("SELECT * FROM songs WHERE title = %s", (title,))
+                song = cur.fetchall()
+
+                return song[0]["lyrics"]["Lyrics"]
+            except:
+                print('[Song Searcher] - Sorry, song search failed. Try again. (ERROR BELOW)')
+                traceback.print_exc()
+                cur.close()
+                conn.rollback()
+        elif sent["Artist"]:
+            try:
+                cur.execute("SELECT FROM songs WHERE artist = %s", (artist,))
+                song = cur.fetchall()
+
+                return song[0]["lyrics"]["Lyrics"]
+            except:
+                print('[Song Searcher] - Sorry, song search failed. Try again. (ERROR BELOW)')
+                traceback.print_exc()
+                cur.close()
+                conn.rollback()
+        else:
+            return "No data received."
+
+    return "OK"
+
+
+@application.route("/lyrics/search")
+def search():
+    query = request.args.get("SEARCH-QUERY")
+    verbose = request.args.get("VERBOSE")
+
+    print('[Song Searcher] - Fetching songs from the database...')
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    cur.execute(
+        "SELECT * FROM songs"
+    )
+    all_songs = cur.fetchall()
+    print('[Song Searcher] - Songs successfully fetched.')
+    print("\n\n---\n\n")
+
+    # Paraphrase Query
+    all_paraphrased = lyrical_paraphraser(query)
+    similarity_score_list = []
+    direct_query_similarity_score_list = []
+
+    for song_index, song in enumerate(all_songs):
+        print('[Song Searcher] - SONG {}: {} - {}'.format(song_index, song['artist'], song['title']))
+        phrases = str(song['lyrics']["Lyrics"]).splitlines()
+        phrases = list(set(phrases))
+        similarity_score_list.append(0)
+        direct_query_similarity_score_list.append(0)
+
+        for index, paraphrased in enumerate(all_paraphrased):
+            for phrase in phrases:
+                similarity = round(lyrical_similarity(phrase, paraphrased) * 100, 2)
+                query_similarity = round(lyrical_similarity(phrase, query) * 100, 2)
+
+                if similarity > 50:
+                    similarity_score_list[song_index] += 1
+
+                    if verbose == "True":
+                        print('[Song Searcher] - ORIGINAL LYRICS: {} | PARAPHRASED LYRICS: {} | '
+                              'QUERY: {}'.format(phrase,
+                                                 paraphrased,
+                                                 query))
+                        print('[Song Searcher] - SIMILARITY: {} %'.format(similarity))
+                        print('[Song Searcher] - For comparison...')
+                        print('[Song Searcher] - QUERY AND ORIGINAL LYRIC SIMILARITY'
+                              ' WITHOUT PARAPHRASING: {} %'.format(query_similarity))
+                        print()
+                elif query_similarity > 50:
+                    direct_query_similarity_score_list[song_index] += 1
+                    if verbose == "True":
+                        print('[Song Searcher] - DIRECT QUERY SIMILARITY WAS OVER 50 % AT: {} %'.format(query_similarity))
+                        print()
+
+        print('[Song Searcher] - SONG RESULTS FOR {}: {} - {}'.format(song_index, song['artist'], song['title']))
+        print('[Song Searcher] - TOTAL POSSIBLE SCORE IS {}.'.format(len(phrases)))
+        print('[Song Searcher] - PARAPHRASING SCORE: {} | '
+              'DIRECT QUERY SCORE: {}'.format(similarity_score_list[song_index],
+                                              direct_query_similarity_score_list[song_index]))
+        better_percentage = round(((similarity_score_list[song_index] - direct_query_similarity_score_list[song_index]) / direct_query_similarity_score_list[song_index]) * 100, 2)
+        print('[Song Searcher] - THIS PROGRAM PROVED TO BE {} % BETTER AT A '
+              'DIRECT SEARCH QUERY.'.format(better_percentage))
+        print()
+
+    return "OK"
+
+
+@application.route("/lyrics/test/paraphrase")
 def paraphrase():
+    global db
     lyrics = request.headers.get("Lyrics_Phrase")
     no_of_variations = int(request.headers.get("Variations"))
 
@@ -50,5 +210,19 @@ def paraphrase():
     return formatted_string
 
 
+# Lyrical Sequence Rater
+def lyrical_similarity(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+# Lyrics Paraphraser
+def lyrical_paraphraser(query):
+    paraphrased_lyrics = paraphrase_lyrics(query, 20)
+
+    return paraphrased_lyrics
+
+
 if __name__ == "__main__":
     application.run(host="0.0.0.0", debug=False)
+
+conn.close()
