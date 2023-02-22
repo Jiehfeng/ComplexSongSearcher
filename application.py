9fc760b7e32ba425676130cb353a0c1a6fab5782
jiehfeng <alainjiehfeng@gmail.com>
2023-02-22 12:50:24 +0530

    Error handling.

--- application.py
+++ application.py
@@ -440,9 +440,8 @@ def batch_submit_func(formatted_lyric_sets):
         print('[SONG SEARCHER] - Paraphrasing lyrics...')
 
         for line in tqdm(unique_lyrics, position=1):
-            if not line or line == "" or line is None or len(line.split()) <= 1:
+            if not line or line == "" or line is None or len(line.split()) <= 2:
                 continue
-            print(line)
             paraphrased_lyrics = paraphrase_lyrics(line, 4)
             for para in paraphrased_lyrics:
                 paraphrased += para + "\n"
